f539fc31ee11c8f6b2c3b85483a2df67831606e0
aleixobianca <biancaaleixoeng@gmail.com>
2024-06-04 21:11:39 -0300

    ajustei a pagina login php

--- backend/db_conexao.php
+++ backend/db_conexao.php
@@ -1,12 +1,21 @@
 <?php
+
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$dbname = "clientes"; 
+$dbname = "clientes";
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+function desconectarBanco() {
+    global $conn;
+    $conn->close();
+}
+
 ?>
--- backend/login.php
+++ backend/login.php
@@ -1,14 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Successfull</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
+    <style>
+
+        .topo-php {
+            background-color: #d2acec;
+            padding: 5px 10px 5px 40px;
+            float: left;
+            width: 100%
+        }
+
+        h2 {
+            display: flex;
+            justify-content: center;
+            text-align: center;
+            align-items: center;
+            color: #000;
+            font-family: "Ubuntu", sans-serif;
+            font-size: 25px;
+            font-style: normal;
+            font-weight: 200;
+            line-height: normal;
+            text-align: center;
+
+        }
+
+        #topo-entrou{
+            justify-content: left;
+            display: flex;
+            width: 50%;
+            float: left;
+        }
+        .btn-usuario-php {
+            justify-content: right;
+            display: flex;
+            width: 50%;
+            float: right;
+        }
+
+        #ir-para-btn {
+            width: 200px;
+            border: none;
+            outline: none;
+            border-radius: 40px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            font-size: 20px;
+            font-weight: 300;
+            background: #25d366;
+            padding: 10px;
+            float: left;
+        }
+
+        #bem-vindo {
+            justify-content: center;
+            padding: 20px 20px 20px 20px;
+            width: 100%;
+            float: left;
+        }
+
+    </style>
+</head>
+<body>
+
 <?php
-$host = 'localhost';
-$db   = 'clientes'; 
-$user = 'root';        
-$pass = '';          
 
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
+require_once 'db_conexao.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
@@ -23,9 +84,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         if ($senha == $row['senha']) {
-            echo "<h2>Entrou no sistema!<h2>";
-            echo "<div class='btn-usuario'>
-            <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a></div>";
+            // echo "<h2>Entrou no sistema!<h2>";
+            // echo "<div class='btn-usuario-php'>
+            // <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a></div>";
+
+            echo "<div class='topo-php'>
+                    <h2 id='topo-entrou'>Entrou no sistema!<h2>
+                    <div class='btn-usuario-php'>
+                        <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                    </div>
+                </div>";
+
+            echo "<h2 id='bem-vindo'>Bem vindo, $email!<h2>";
+
         } else {
             echo "Senha incorreta.";
         }
@@ -46,42 +117,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
 }
 
-$conn->close();
+desconectarBanco();
+
 ?>
 
-<style>
-    h2 {
-        color: #000;
-        font-family: "Ubuntu", sans-serif;
-        font-size: 25px;
-        font-style: normal;
-        font-weight: 200;
-        line-height: normal;
-        text-align: center;
-        width: 100%;
-        margin-top: 140px;
-        float: left;
-    }
-    #ir-para-btn {
-        width: 200px;
-        border: none;
-        outline: none;
-        border-radius: 40px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        cursor: pointer;
-        font-size: 25px;
-        font-weight: 600;
-        background: #25d366;
-        margin-top: 20px;
-        padding: 10px;
-        float: left;
-    }
+</body>
+</html>
 
-    .btn-usuario {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        float: left;
-    }
-</style>
+
+
