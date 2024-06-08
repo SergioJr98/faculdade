7e673e1ce74f8d78ac14db0b64e2210ea26b8123
Bianca Aleixo <161977269+aleixobianca@users.noreply.github.com>
2024-06-08 00:16:14 -0300

    atualização do css e mensagens backend

--- backend/login.php
+++ backend/login.php
@@ -1,133 +1,79 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Successfull</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
-    <style>
-
-        .topo-php {
-            background-color: #d2acec;
-            padding: 5px 10px 5px 40px;
-            float: left;
-            width: 100%
-        }
-
-        h2 {
-            display: flex;
-            justify-content: center;
-            text-align: center;
-            align-items: center;
-            color: #000;
-            font-family: "Ubuntu", sans-serif;
-            font-size: 25px;
-            font-style: normal;
-            font-weight: 200;
-            line-height: normal;
-            text-align: center;
-
-        }
-
-        #topo-entrou{
-            justify-content: left;
-            display: flex;
-            width: 50%;
-            float: left;
-        }
-        .btn-usuario-php {
-            justify-content: right;
-            display: flex;
-            width: 50%;
-            float: right;
-        }
-
-        #ir-para-btn {
-            width: 200px;
-            border: none;
-            outline: none;
-            border-radius: 40px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            cursor: pointer;
-            font-size: 20px;
-            font-weight: 300;
-            background: #76edb4;
-            padding: 10px;
-            float: left;
-        }
-
-        #ir-para-btn:hover {
-            border: 2px outset #bfe2c1;
-            background: #6bd1a0;
-        }
-
-        #bem-vindo {
-            justify-content: center;
-            padding: 20px 20px 20px 20px;
-            width: 100%;
-            float: left;
-        }
-
-    </style>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
 </head>
+
 <body>
 
-<?php
+    <?php
 
-require_once 'db_conexao.php';
+        require_once 'db_conexao.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuarios WHERE email = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+            $sql = "SELECT * FROM usuarios WHERE email = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        if ($senha == $row['senha']) {
-            // echo "<h2>Entrou no sistema!<h2>";
-            // echo "<div class='btn-usuario-php'>
-            // <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a></div>";
+            //-----código antigo---------
+            // if ($result->num_rows > 0) {
+            //     $row = $result->fetch_assoc();
+            //     if (password_verify($senha, $row['senha'])) {
+            //         echo "Entrou no sistema!";
+            //     } else {
+            //         echo "Senha incorreta.";
+            //     }
+            // } else {
+            //     echo "Usuário não encontrado.";
+            // }
+            //-----fim código antigo--------
 
-            echo "<div class='topo-php'>
-                    <h2 id='topo-entrou'>Entrou no sistema!<h2>
-                    <div class='btn-usuario-php'>
-                        <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
-                    </div>
-                </div>";
 
-            echo "<h2 id='bem-vindo'>Bem vindo, $email!<h2>";
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                if ($senha == $row['senha']) {
+                    echo "<div class='topo-php'>
+                            <h2 id='topo-msg'>Entrou no sistema! \u{2705}<h2>
+                            <div class='btn-usuario-php'>
+                                <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                            </div>
+                        </div>";
 
-        } else {
-            echo "Senha incorreta.";
+                    echo "<h2 id='bem-vindo'>Bem vindo, $email!<h2>";
+
+                } else {
+                    // echo "Senha incorreta \u{274C}";
+                    echo "<div class='topo-php'>
+                            <h2 id='topo-msg'>Senha incorreta \u{274C}<h2>
+                            <div class='btn-usuario-php'>
+                                <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                            </div>
+                        </div>";
+                }
+            } else {
+                echo "<div class='topo-php'>
+                            <h2 id='topo-msg'>Usuário não encontrado \u{274C}<h2>
+                            <div class='btn-usuario-php'>
+                                <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                            </div>
+                        </div>";
+            }  
+
+            $stmt->close();
         }
-    } else {
-        echo "Usuário não encontrado.";
-    }
 
-    // if ($result->num_rows > 0) {
-    //     $row = $result->fetch_assoc();
-    //     if (password_verify($senha, $row['senha'])) {
-    //         echo "Entrou no sistema!";
-    //     } else {
-    //         echo "Senha incorreta.";
-    //     }
-    // } else {
-    //     echo "Usuário não encontrado.";
-    // }
-    $stmt->close();
-}
-
-desconectarBanco();
-
-?>
-
+        desconectarBanco();
+    ?>
 </body>
+
 </html>
-
-
-
--- backend/remover_usuario.php
+++ backend/remover_usuario.php
@@ -1,134 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Remoção usuário</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
-    <style>
-
-        .topo-php {
-            background-color: #d2acec;
-            padding: 5px 10px 5px 40px;
-            float: left;
-            width: 100%
-        }
-
-        h2 {
-            display: flex;
-            justify-content: center;
-            text-align: center;
-            align-items: center;
-            color: #000;
-            font-family: "Ubuntu", sans-serif;
-            font-size: 25px;
-            font-style: normal;
-            font-weight: 200;
-            line-height: normal;
-            text-align: center;
-
-        }
-
-        #topo-entrou{
-            justify-content: left;
-            display: flex;
-            width: 50%;
-            float: left;
-        }
-        .btn-usuario-php {
-            justify-content: right;
-            display: flex;
-            width: 50%;
-            float: right;
-        }
-
-
-        #ir-para-btn {
-            width: 200px;
-            border: none;
-            outline: none;
-            border-radius: 40px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            cursor: pointer;
-            font-size: 20px;
-            font-weight: 300;
-            background: #76edb4;
-            padding: 10px;
-            float: left;
-        }
-        
-        #ir-para-btn:hover {
-            border: 2px outset #bfe2c1;
-            background: #6bd1a0;
-        }
-
-    </style>
 </head>
+
 <body>
 
-<?php
+    <?php
     
-    require_once 'db_conexao.php';
+        require_once 'db_conexao.php';
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = $_POST['email-remover'];
-        $senha = $_POST['senha-remover'];
-        
-        $sql = "SELECT senha FROM usuarios WHERE email = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST['email-remover'];
+            $senha = $_POST['senha-remover'];
+            
+            $sql = "SELECT senha FROM usuarios WHERE email = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            if ($senha == $row['senha']) {
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                if ($senha == $row['senha']) {
 
-                $sql = "DELETE FROM usuarios WHERE email = '$email'";
-                
-                if ($conn->query($sql) === TRUE) {
+                    $sql = "DELETE FROM usuarios WHERE email = '$email'";
+                    
+                    if ($conn->query($sql) === TRUE) {
+                        echo "<div class='topo-php'>
+                                <h2 id='topo-msg'>Conta removida com sucesso! \u{2705}<h2>
+                                <div class='btn-usuario-php'>
+                                    <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                                </div>
+                            </div>";
+
+                    } else {
+                        echo "Erro: " . $sql . "<br>" . $conn->error;
+                    }
+
+                } else {
                     echo "<div class='topo-php'>
-                            <h2 id='topo-entrou'>Conta removida com sucesso!<h2>
+                            <h2 id='topo-msg'>Senha incorreta \u{274C}<h2>
                             <div class='btn-usuario-php'>
                                 <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
                             </div>
                         </div>";
-
-                } else {
-                    echo "Erro: " . $sql . "<br>" . $conn->error;
                 }
-
             } else {
-                echo "Senha incorreta.";
+                echo "<div class='topo-php'>
+                            <h2 id='topo-msg'>Usuário não encontrado \u{274C}<h2>
+                            <div class='btn-usuario-php'>
+                                <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                            </div>
+                        </div>";
             }
-        } else {
-            echo "Usuário não encontrado.";
+            
         }
-
-        // if ($result->num_rows > 0) {
-        //     $row = $result->fetch_assoc();
-        //     if (password_verify($senha, $row['senha'])) {
-
-        //         $senha_criptografada = password_hash($nova_senha, PASSWORD_BCRYPT);
-
-        //         $sql = "UPDATE usuarios SET senha = '$senha_criptografada', email = '$novo_email' WHERE cpf = '$cpf'";
-                
-        //         if ($conn->query($sql) === TRUE) {
-        //             echo "Dados atualizados com sucesso!";
-        //         } else {
-        //             echo "Erro: " . $sql . "<br>" . $conn->error;
-        //         }
-
-        //     } else {
-        //         echo "Senha incorreta.";
-        //     }
-        // } else {
-        //     echo "Usuário não encontrado.";
-        // }
-
-        
-    }
-    desconectarBanco();
-?>
+        desconectarBanco();
+    ?>
 </body>
+
 </html>
