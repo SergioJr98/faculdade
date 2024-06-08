1b0d65123a2a00f154c5bbc5c4ef19d9eaf44501
Bianca Aleixo <161977269+aleixobianca@users.noreply.github.com>
2024-06-08 13:48:46 -0300

    fiz o tratamento do erro para tentativa de registro de usuário já cadastrado

--- backend/registro.php
+++ backend/registro.php
@@ -14,7 +14,8 @@
     <?php
 
         require_once 'db_conexao.php';
-
+        mysqli_report(MYSQLI_REPORT_STRICT|MYSQLI_REPORT_ERROR);
+        
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $email = $_POST['email'];
             $senha = $_POST['senha'];
@@ -26,19 +27,38 @@
 
             // $sql = "INSERT INTO usuarios (email, senha, cpf) VALUES ('$email', '$senha_criptografada', '$cpf')";
             //----------fim código antigo------------
+            try {
+                $sql = "INSERT INTO usuarios (email, senha, cpf) VALUES ('$email', '$senha', '$cpf')";
 
-            $sql = "INSERT INTO usuarios (email, senha, cpf) VALUES ('$email', '$senha', '$cpf')";
-
-            if ($conn->query($sql) === TRUE) {
+                if ($conn->query($sql) === TRUE) {
+                    echo "<div class='topo-php'>
+                            <h2 id='topo-msg'>Registro realizado com sucesso! \u{2705}<h2>
+                            <div class='btn-usuario-php'>
+                                <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
+                            </div>
+                        </div>";
+                } else {
+                    echo "Erro: " . $sql . "<br>" . $conn->error;
+                }
+            }  catch (mysqli_sql_exception $e) {
                 echo "<div class='topo-php'>
-                        <h2 id='topo-msg'>Registro realizado com sucesso! \u{2705}<h2>
+                        <h2 id='topo-msg'>Erro no registro \u{274C}<h2>
                         <div class='btn-usuario-php'>
                             <a href='../usuario.html'><button type='button' id='ir-para-btn'>Voltar</button></a>
                         </div>
                     </div>";
-            } else {
-                echo "Erro: " . $sql . "<br>" . $conn->error;
+
+                if ($e->getCode() == 1062) {
+                    // Tratamento específico para duplicidade de entrada para chave única
+                    echo "<p>E-mail ou CPF já cadastrado!</p>";
+                    echo "<p id='mensagem-exception'>Erro: " . $e->getMessage(). "</p>";
+
+                } else {
+                    // Tratamento para outros erros
+                    echo "<p id='mensagem-exception'>Erro MySQL: " . $e->getMessage() . " (Código: " . $e->getCode() . ")<p>";
+                }
             }
+
         }
 
         desconectarBanco();
